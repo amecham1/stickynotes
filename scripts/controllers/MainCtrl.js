675d19a6176ad00c8baa90e93f5b13b5998f81a6
Andrew <andrew.mecham@gmail.com>
2016-07-15 12:33:11 -0600

    finished project

--- scripts/controllers/MainCtrl.js
+++ scripts/controllers/MainCtrl.js
@@ -6,10 +6,18 @@ function MainCtrl($scope,noteService) {
 
 
 $scope.saveNote = function(newNote){
+  $scope.note = " ";
 noteService.addNote(newNote);
+getNotes();
+};
+
+function getNotes(){
+  noteService.getNotes().then (function(response){
+    $scope.notes = response;
+
+  });
 
 }
-
-
+getNotes();
 
 }//end of controller
--- scripts/services/noteService.js
+++ scripts/services/noteService.js
@@ -2,13 +2,23 @@ angular.module("notesApp")
 .service("noteService", noteService);
 
 
-function noteService(){
+function noteService($q){
 
 this.addNote = function(note){
-  localStorage.setItem(JSON.stringify(note));
+  var existingEntries = JSON.parse(localStorage.getItem("notes"));
+  if(!existingEntries){
+    existingEntries = [];
+  }
+  existingEntries.push(note);
+  localStorage.setItem("notes", JSON.stringify(existingEntries));
 };
 
+this.getNotes = function(){
+  var deferred = $q.defer();
+  deferred.resolve(JSON.parse(localStorage.getItem("notes")));
 
+  return deferred.promise;
+};
 
 
 
